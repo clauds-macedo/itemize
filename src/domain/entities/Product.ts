9e5a485af0a5472d4213909a5d965123aec3cc91
clauds-macedo <yanalogic@gmail.com>
2024-08-29 22:24:14 -0300

    feat: add Product entity

---
+++ src/domain/entities/Product.ts
@@ -0,0 +1,16 @@
+interface Rating {
+  rate: number
+  count: number
+}
+
+export class Product {
+  constructor(
+    public readonly id: number,
+    public readonly title: string,
+    public readonly price: number,
+    public readonly description: string,
+    public readonly category: string,
+    public readonly image: string,
+    public readonly rating: Rating,
+  ) {}
+}

0aa8d8459c8219f297a138bd5281abc43521e014
clauds-macedo <yanalogic@gmail.com>
2024-08-30 07:34:20 -0300

    feat: render products on Home page

--- src/presentation/pages/Home/index.tsx
+++ src/presentation/pages/Home/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from 'react-native';
-import { Card } from './components/Card';
+import { colors } from '@presentation/global/colors';
+import { FlatList, View } from 'react-native';
+import { Card } from '../../components/Card';
 import { useProducts } from './hooks/useProducts';
 
 export const Home: React.FC = () => {
   const { products } = useProducts();
-  console.log(products);
+
   return (
-    <View style={{ flex: 1, alignItems: 'center' }}>
-      <Card.Root>
-        <Text>oi</Text>
-      </Card.Root>
+    <View
+      style={{
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: colors.primary.background,
+      }}
+    >
+      <FlatList
+        data={products}
+        style={{ width: '100%' }}
+        numColumns={2}
+        contentContainerStyle={{
+          gap: 8,
+          padding: 16,
+        }}
+        renderItem={({ item }) => (
+          <Card.Root style={{ margin: 4 }} key={item.id}>
+            <Card.Image source={{ uri: item.image }} />
+            <Card.Text>{item.title}</Card.Text>
+          </Card.Root>
+        )}
+      />
     </View>
   );
 };

051cf63e1e24ac7e0b4c52f11adb7e6a15e4dc61
clauds-macedo <yanalogic@gmail.com>
2024-08-29 23:42:09 -0300

    feat: add routes

---
+++ src/presentation/routes/index.tsx
@@ -0,0 +1,16 @@
+import { Home } from '@presentation/pages/Home';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import type { RootStackParamList } from './types';
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};

0e26d82e852a0e0308e86640ac9e1f01c9e2efc2
clauds-macedo <yanalogic@gmail.com>
2024-08-31 22:26:17 -0300

    feat: create card button

---
+++ src/presentation/components/Card/CardButton.tsx
@@ -0,0 +1,13 @@
+type CardButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+export const CardButton: React.FC<CardButtonProps> = ({
+  className,
+  ...props
+}) => {
+  return (
+    <button
+      className={`bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center ${className}`}
+      {...props}
+    />
+  );
+};

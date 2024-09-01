05331e204b4e6735868498d50edcd1ae0faa9094
clauds-macedo <yanalogic@gmail.com>
2024-08-31 21:48:31 -0300

    feat: add index for Card components

---
+++ src/presentation/components/Card/index.tsx
@@ -0,0 +1,15 @@
+import { CardActions } from './CardActions';
+import { CardImage } from './CardImage';
+import { CardPrice } from './CardPrice';
+import { CardRating } from './CardRating';
+import { CardRoot } from './CardRoot';
+import { CardTitle } from './CardTitle';
+
+export const Card = {
+  Root: CardRoot,
+  Image: CardImage,
+  Title: CardTitle,
+  Price: CardPrice,
+  Rating: CardRating,
+  Actions: CardActions,
+};

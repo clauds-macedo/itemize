5108eb0f8b2752b3957b0827b2c0cf23be72bc7d
clauds-macedo <yanalogic@gmail.com>
2024-09-02 16:18:30 -0300

    test: add ProductsRepository test

---
+++ src/__tests__/infra/repositories/ProductsRepository.spec.ts
@@ -0,0 +1,62 @@
+import { fakeProduct } from '@/__tests__/__mocks__/fakes/Product';
+import type { Product } from '@/domain/entities/Product';
+import type { IHttpClientRepository } from '@/domain/repositories/IHttpClientRepository';
+import { GetHttpClientUseCase } from '@/domain/usecases/GetHttpClientUseCase';
+import { ProductRepository } from '@/infra/repositories/ProductsRepository';
+
+const mockHttpClientRepository: jest.Mocked<IHttpClientRepository> = {
+  get: jest.fn(),
+};
+
+const getHttpClientUseCase = new GetHttpClientUseCase(mockHttpClientRepository);
+const productRepository = new ProductRepository(getHttpClientUseCase);
+
+describe('ProductRepository', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return a list of products', async () => {
+    const mockProducts: Product[] = [fakeProduct, { ...fakeProduct, id: 111 }];
+
+    mockHttpClientRepository.get.mockResolvedValue({
+      data: mockProducts,
+      status: 200,
+    });
+
+    const products = await productRepository.getProducts();
+
+    expect(products).toEqual(mockProducts);
+    expect(mockHttpClientRepository.get).toHaveBeenCalledWith('/products');
+  });
+
+  it('should throw an error when the request fails', async () => {
+    mockHttpClientRepository.get.mockRejectedValue(new Error('Request failed'));
+
+    await expect(productRepository.getProducts()).rejects.toThrow(
+      'Failed to fetch products',
+    );
+    expect(mockHttpClientRepository.get).toHaveBeenCalledWith('/products');
+  });
+
+  it('should return a single product by id', async () => {
+    mockHttpClientRepository.get.mockResolvedValue({
+      data: fakeProduct,
+      status: 200,
+    });
+
+    const product = await productRepository.getProductById(1);
+
+    expect(product).toEqual(fakeProduct);
+    expect(mockHttpClientRepository.get).toHaveBeenCalledWith('/products/1');
+  });
+
+  it('should throw an error when the request fails', async () => {
+    mockHttpClientRepository.get.mockRejectedValue(new Error('Request failed'));
+
+    await expect(productRepository.getProductById(1)).rejects.toThrow(
+      'Failed to fetch product with id 1',
+    );
+    expect(mockHttpClientRepository.get).toHaveBeenCalledWith('/products/1');
+  });
+});
